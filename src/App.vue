71a527a3abe5f389cf998b8150d9898d3de1bd81
CN\YANGSS5 <yangss5@meicloud.com>
2021-03-03 08:56:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="text-4xl bg-teal-500 text-orange-500">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam molestias saepe est, animi, nemo nisi velit quos dicta iste deleniti in distinctio unde adipisci ratione, provident autem? Porro, cumque nihil!
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import 'windi.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
